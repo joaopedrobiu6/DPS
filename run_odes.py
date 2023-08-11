769053a142fd7d53b2d445c9951f6dc11a79d71d
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-08-11 02:31:10 +0100

    made some comments

--- run_odes.py
+++ run_odes.py
@@ -14,6 +14,7 @@ if model == 'guiding-center':
 from jax.config import config
 config.update("jax_enable_x64", True)
 
+# This runs the solver and how long does it take to solve the system
 def compute_and_time(func):
     start = time.time()
     result = func()
@@ -26,14 +27,16 @@ def main(results_path='results'):
         "JAX": (solve_with_jax, compute_jacobian_jax)
     }
     
+    # Selects the models from solver_models if they are also in solver_functions
     selected_solver_models = [model for model in solver_models if model in solver_functions]
+    # Makes solver_functions equal to the selected ones 
     solver_functions = {model: solver_functions[model] for model in selected_solver_models}
 
+    # Separates the ODE solvers from the Jacobian calculation functions
     solvers = [solver_functions.get(solver, (None, None))[0] for solver in solver_models]
     jacobi_computers = [solver_functions.get(solver, (None, None))[1] for solver in solver_models]
 
     w_solvers, times_solve = zip(*[compute_and_time(solver) for solver in solvers])
-    print(times_solve)
     Jacobian_solvers, times_jacobi = zip(*[compute_and_time(jacobian) for jacobian in jacobi_computers])
 
     diffs = compute_diff(w_solvers, Jacobian_solvers, list(solver_functions.keys()))
